a749d8833866cdf082407e8a5bdf1f62c531b9ad
jonahanton <jonahlouisanton@gmail.com>
2022-05-25 18:01:00 +0100

    minor modifications

--- models/mst.py
+++ models/mst.py
@@ -1,5 +1,5 @@
 """
-A PyTorch implementation of Vision Transformers [Dosovitskiy et al., 2020], 
+A PyTorch implementation of Vision Transformers [Dosovitskiy et al., 2020],
 adapted for our purposes (appropriate for spectrogram input, w/ masking).
 References:
 	https://github.com/facebookresearch/mae/blob/main/models_mae.py
@@ -8,17 +8,19 @@ References:
 	https://github.com/YuanGongND/ast/blob/master/src/models/ast_models.py
 	https://github.com/YuanGongND/ssast/blob/main/src/models/ast_models.py#L76
 """
-import torch 
+import torch
 import torch.nn as nn
 
 import timm
 from timm.models.vision_transformer import PatchEmbed, Block
 
+from functools import partial
+
 
 class MaskedSpectrogramTransformer(nn.Module):
-	def __init__(
-		img_size=(64,96),
-		patch_size=(16,16),
+    def __init__(
+        img_size=(64, 96),
+		patch_size=(16, 16),
 		in_chans=1,
 		embed_dim=768,
 		depth=12,
@@ -26,41 +28,39 @@ class MaskedSpectrogramTransformer(nn.Module):
 		mlp_ratio=4.,
 		norm_layer=nn.LayerNorm,
 	):
-		super().__init__()
-		
+        super().__init__()
+
 		# projects input to have dim. embed_dim (conv2d layer, kernel_size=patch_size, stride=patch_size)
 		self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
 		num_patches = self.patch_embed.num_patches
-		
+
 		# [CLS] token
 		self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
 		# (learned) positional embeddings
 		self.pos_embed = nn.Parameter(torch.zeros(1, num_patches+1, embed_dim))
-		
+
 		self.blocks = nn.ModuleList([
-			Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+			Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True,
+			      qk_scale=None, norm_layer=norm_layer)
 			for i in range(depth)])
-			
+
 		self.norm = norm_layer(embed_dim)
-		
+
 		# timm's trunc_normal_(std=.02) is effectively normal_(std=.02) as cutoff is too big (2.)
         torch.nn.init.normal_(self.cls_token, std=.02)
-        torch.nn.init.normal_(self.pos_embed, std=.02)
-        self.apply(self._init_weights)
-        		
-        
+		torch.nn.init.normal_(self.pos_embed, std=.02)
+		self.apply(self._init_weights)
+
+                
 	def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        elif isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
-            if isinstance(m, nn.Conv2d) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
+		if isinstance(m, nn.Linear):
+			# we use xavier_uniform following official JAX ViT:
+			torch.nn.init.xavier_uniform_(m.weight)
+			if isinstance(m, nn.Linear) and m.bias is not None:
+				nn.init.constant_(m.bias, 0)
+		elif isinstance(m, nn.LayerNorm):
+			nn.init.constant_(m.bias, 0)
+			nn.init.constant_(m.weight, 1.0)
        
 		
 	def random_masking(self, x, mask_ratio):
@@ -73,15 +73,15 @@ class MaskedSpectrogramTransformer(nn.Module):
 		
 		if isinstance(mask_ratio, (torch.Tensor, np.ndarray, list, tuple)):
 			# Prefixed mask
-            mask = mask_ratio.clone().detach()
-            ids_shuffle = torch.argsort(mask.reshape(N, -1), dim=1)
-            ids_restore = torch.argsort(ids_shuffle, dim=1)
-            len_keep = (mask[0] == 0).sum()
-        elif mask_ratio == 0:
-            # No mask
-            mask = torch.zeros([N, L], device=x.device)
-            ids_restore = torch.tensor(list(range(L))).to(torch.int)
-            return x, mask, ids_restore
+			mask = mask_ratio.clone().detach()
+			ids_shuffle = torch.argsort(mask.reshape(N, -1), dim=1)
+			ids_restore = torch.argsort(ids_shuffle, dim=1)
+			len_keep = (mask[0] == 0).sum()
+		elif mask_ratio == 0:
+			# No mask
+			mask = torch.zeros([N, L], device=x.device)
+			ids_restore = torch.tensor(list(range(L))).to(torch.int)
+			return x, mask, ids_restore
         else:
             # Random mask
             len_keep = int(L * (1 - mask_ratio))
