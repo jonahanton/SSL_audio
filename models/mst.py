618b3f669a6a2bc9c7ab18e4a75728dde3504596
Jonah Anton <88099788+jonahanton@users.noreply.github.com>
2022-05-25 18:03:26 +0100

    Update mst.py

--- models/mst.py
+++ models/mst.py
@@ -47,7 +47,7 @@ class MaskedSpectrogramTransformer(nn.Module):
 		self.norm = norm_layer(embed_dim)
 
 		# timm's trunc_normal_(std=.02) is effectively normal_(std=.02) as cutoff is too big (2.)
-        torch.nn.init.normal_(self.cls_token, std=.02)
+        	torch.nn.init.normal_(self.cls_token, std=.02)
 		torch.nn.init.normal_(self.pos_embed, std=.02)
 		self.apply(self._init_weights)
 
@@ -82,29 +82,29 @@ class MaskedSpectrogramTransformer(nn.Module):
 			mask = torch.zeros([N, L], device=x.device)
 			ids_restore = torch.tensor(list(range(L))).to(torch.int)
 			return x, mask, ids_restore
-        else:
-            # Random mask
-            len_keep = int(L * (1 - mask_ratio))
-            noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
-            # sort noise for each sample
-            ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-            ids_restore = torch.argsort(ids_shuffle, dim=1)
+		else:
+		    # Random mask
+		    len_keep = int(L * (1 - mask_ratio))
+		    noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+		    # sort noise for each sample
+		    ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+		    ids_restore = torch.argsort(ids_shuffle, dim=1)
         
-        # keep the first subset
-        ids_keep = ids_shuffle[:,:len_keep]
-        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1,1,D))
+        	# keep the first subset
+        	ids_keep = ids_shuffle[:,:len_keep]
+        	x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1,1,D))
         
-        # generate the binary mask: 0 is keep, 1 is remove
-        mask = torch.ones([N, L], device=x.device)
-        mask[:, :len_keep] = 0
-        # unshuffle to get the binary mask
-        mask = torch.gather(mask, dim=1, index=ids_restore)
+        	# generate the binary mask: 0 is keep, 1 is remove
+        	mask = torch.ones([N, L], device=x.device)
+        	mask[:, :len_keep] = 0
+        	# unshuffle to get the binary mask
+       		mask = torch.gather(mask, dim=1, index=ids_restore)
         
-        return x_masked, mask, ids_restore
+        	return x_masked, mask, ids_restore
 		
     
     
-    def forward(self, imgs, mask_ratio=0.75):
+	def forward(self, imgs, mask_ratio=0.75):
 		# patchify + embed imgs
 		x = self.patch_embed(imgs)
 		
