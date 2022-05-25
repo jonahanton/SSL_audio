4ce778de380bd25077099af6703a2163e789b455
jonahanton <jonahlouisanton@gmail.com>
2022-05-25 18:08:43 +0100

    debugging mst

--- models/mst.py
+++ models/mst.py
@@ -52,89 +52,89 @@ class MaskedSpectrogramTransformer(nn.Module):
 		self.apply(self._init_weights)
 
                 
-	def _init_weights(self, m):
-		if isinstance(m, nn.Linear):
-			# we use xavier_uniform following official JAX ViT:
-			torch.nn.init.xavier_uniform_(m.weight)
-			if isinstance(m, nn.Linear) and m.bias is not None:
-				nn.init.constant_(m.bias, 0)
-		elif isinstance(m, nn.LayerNorm):
-			nn.init.constant_(m.bias, 0)
-			nn.init.constant_(m.weight, 1.0)
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+	    torch.nn.init.xavier_uniform_(m.weight)
+	    if isinstance(m, nn.Linear) and m.bias is not None:
+		nn.init.constant_(m.bias, 0)
+	elif isinstance(m, nn.LayerNorm):
+	    nn.init.constant_(m.bias, 0)
+	    nn.init.constant_(m.weight, 1.0)
        
 		
-	def random_masking(self, x, mask_ratio):
-		"""
-		Perform per-sample random masking by per-sample shuffling.
-		Per-sample shuffling is done by argsort random noise.
-		x: [N, L, D], sequence
-		"""
-		N, L, D = x.shape  # batch, length, dim
+    def random_masking(self, x, mask_ratio):
+    """
+    Perform per-sample random masking by per-sample shuffling.
+    Per-sample shuffling is done by argsort random noise.
+    x: [N, L, D], sequence
+    """
+    N, L, D = x.shape  # batch, length, dim
 		
-		if isinstance(mask_ratio, (torch.Tensor, np.ndarray, list, tuple)):
-			# Prefixed mask
-			mask = mask_ratio.clone().detach()
-			ids_shuffle = torch.argsort(mask.reshape(N, -1), dim=1)
-			ids_restore = torch.argsort(ids_shuffle, dim=1)
-			len_keep = (mask[0] == 0).sum()
-		elif mask_ratio == 0:
-			# No mask
-			mask = torch.zeros([N, L], device=x.device)
-			ids_restore = torch.tensor(list(range(L))).to(torch.int)
-			return x, mask, ids_restore
-		else:
-		    # Random mask
-		    len_keep = int(L * (1 - mask_ratio))
-		    noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
-		    # sort noise for each sample
-		    ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-		    ids_restore = torch.argsort(ids_shuffle, dim=1)
+    if isinstance(mask_ratio, (torch.Tensor, np.ndarray, list, tuple)):
+    # Prefixed mask
+    mask = mask_ratio.clone().detach()
+    ids_shuffle = torch.argsort(mask.reshape(N, -1), dim=1)
+    ids_restore = torch.argsort(ids_shuffle, dim=1)
+    len_keep = (mask[0] == 0).sum()
+    elif mask_ratio == 0:
+        # No mask
+        mask = torch.zeros([N, L], device=x.device)
+	ids_restore = torch.tensor(list(range(L))).to(torch.int)
+	return x, mask, ids_restore
+    else:
+	# Random mask
+	len_keep = int(L * (1 - mask_ratio))
+	noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+	# sort noise for each sample
+	ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+	ids_restore = torch.argsort(ids_shuffle, dim=1)
         
-        	# keep the first subset
-        	ids_keep = ids_shuffle[:,:len_keep]
-        	x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1,1,D))
+        # keep the first subset
+        ids_keep = ids_shuffle[:,:len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1,1,D))
         
-        	# generate the binary mask: 0 is keep, 1 is remove
-        	mask = torch.ones([N, L], device=x.device)
-        	mask[:, :len_keep] = 0
-        	# unshuffle to get the binary mask
-       		mask = torch.gather(mask, dim=1, index=ids_restore)
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+       	mask = torch.gather(mask, dim=1, index=ids_restore)
         
-        	return x_masked, mask, ids_restore
+        return x_masked, mask, ids_restore
 		
     
     
-	def forward(self, imgs, mask_ratio=0.75):
-		# patchify + embed imgs
-		x = self.patch_embed(imgs)
+    def forward(self, imgs, mask_ratio=0.75):
+        # patchify + embed imgs
+	x = self.patch_embed(imgs)
 		
-		# add pos embed w/o cls token
-		x = x + self.pos_embed[:,1:,:]
+	# add pos embed w/o cls token
+	x = x + self.pos_embed[:,1:,:]
 		
-		# masking: length -> length * mask_ratio
-		x, mask, ids_restore = self.random_masking(x, mask_ratio)
+	# masking: length -> length * mask_ratio
+	x, mask, ids_restore = self.random_masking(x, mask_ratio)
 		
-		# append cls token
-		cls_token = self.cls_token + self.pos_embed[:,:1,:]
-		cls_token = cls_token.expand(x.shape[0], -1, -1)  # expand dim 0 of cls token to equal batch size
-		x = torch.cat((cls_tokens, x), dim=1)
+	# append cls token
+	cls_token = self.cls_token + self.pos_embed[:,:1,:]
+	cls_token = cls_token.expand(x.shape[0], -1, -1)  # expand dim 0 of cls token to equal batch size
+	x = torch.cat((cls_tokens, x), dim=1)
 		
-		# apply Transformer blocks
-		for blk in self.blocks:
-			x = blk(x)
-		x = self.norm(x)
+	# apply Transformer blocks
+	for blk in self.blocks:
+	    x = blk(x)
+	x = self.norm(x)
 		
-		# return cls token global clip representation
-		x = x[:, 0]
+	# return cls token global clip representation
+	x = x[:, 0]
 		
-		return x, mask, ids_restore
+	return x, mask, ids_restore
 		
 	
 
 
 
 def mst_vit_base_p16x16(patch_size=(16,16), **kwargs):
-	model = MaskedSpectrogramTransformer(
-		patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
-		qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-	return model
+    model = MaskedSpectrogramTransformer(
+	patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
+	qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
