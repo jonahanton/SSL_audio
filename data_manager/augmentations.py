05a8f06a9bc742e1647ca7e6a805227c28bd1e82
jonahanton <jonahlouisanton@gmail.com>
2022-05-30 12:00:04 +0100

    Testing audio augmentations

--- data_manager/augmentations.py
+++ data_manager/augmentations.py
@@ -1,6 +1,6 @@
 """
 
-Augmentations on the log mel spectrogram (i.e., in the time-frequency domain). 
+Audio augmentation modules.
 
 Key:
 	F: Number of frequency bins.
@@ -40,6 +40,10 @@ class Mixup(nn.Module):
 		
 		
 	def forward(self, x):
+
+		if isinstance(x, np.ndarray):
+			x = torch.tensor(x)
+
 		# mix random
 		alpha = self.ratio * np.random.random()
 		if self.memory_bank:
@@ -55,16 +59,21 @@ class Mixup(nn.Module):
 		return mixed.to(torch.float)
 
 
-class MixGaussianNoise():
+class MixGaussianNoise(nn.Module):
 	"""
 	Mix with random (Gaussian) noise.
 	"""
 
 	def __init__(self, ratio=0.4):
+		super().__init__()
 		self.ratio = ratio
 		
 	
 	def forward(self, x):
+
+		if isinstance(x, np.ndarray):
+			x = torch.tensor(x)
+		
 		lambd = self.ratio * np.random.random()
 		# create random gaussian noise
 		z = torch.normal(0, lambd, x.shape)
@@ -75,3 +84,41 @@ class MixGaussianNoise():
 		return mixed
 		
 	
+
+if __name__ == "__main__":
+
+	unit_length = int(0.95 * 16000)
+	mixup = Mixup()
+	gaussnoise = MixGaussianNoise()
+
+
+	wav1, sr = torchaudio.load('data/audioset/samples/--1yd6dcNOQ.wav')
+	wav2, sr = torchaudio.load('data/audioset/samples/MWTJo7DaBZQ.wav')
+	wavs = [wav1, wav2]
+	processed_wavs = []
+	for wav in wavs:
+		# if audio has 2 channels, convert to mono
+		if wav.shape[0] == 2:
+			wav = torch.mean(wav, dim=0).unsqueeze(0)
+		wav = wav[0]  # (1, length) -> (length,)
+
+		# zero padding to both ends
+		length_adj = unit_length - len(wav)
+		if length_adj > 0:
+			half_adj = length_adj // 2
+			wav = F.pad(wav, (half_adj, length_adj - half_adj))
+
+		# random crop unit length wave
+		length_adj = len(wav) - unit_length
+		start = random.randint(0, length_adj) if length_adj > 0 else 0
+		wav = wav[start:start + unit_length]
+
+		processed_wavs.append(wav)
+	
+	wav1, wav2 = processed_wavs
+	torchaudio.save('wav1.wav', wav1.unsqueeze(0), sample_rate=16000)
+	torchaudio.save('wav2.wav', wav2.unsqueeze(0), sample_rate=16000)
+	wav1_gauss = gaussnoise(wav1)
+	wav2_gauss = gaussnoise(wav2)
+	torchaudio.save('wav1_noisy.wav', wav1_gauss.unsqueeze(0), sample_rate=16000)
+	torchaudio.save('wav2_noisy.wav', wav2_gauss.unsqueeze(0), sample_rate=16000)
--- data_manager/transforms.py
+++ data_manager/transforms.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
+import torchaudio
+
+import random 
 
 from audiomentations import Compose, TimeStretch, PitchShift
-from data_manager.augmentations_lms import Mixup, MixGaussianNoise
+from data_manager.augmentations import Mixup, MixGaussianNoise
 
 
 def make_transforms(cfg):
@@ -31,3 +34,36 @@ def make_transforms(cfg):
 	
 	return wav_transform, lms_transform
 	
+
+if __name__ == "__main__":
+
+	from utils import utils
+	cfg = utils.load_yaml_config('config.yaml')
+	wav_transform, lms_transform = make_transforms(cfg)
+
+	unit_length = int(0.95 * 16000)
+	wav, sr = torchaudio.load('data/audioset/samples/--1yd6dcNOQ.wav')
+
+	# if audio has 2 channels, convert to mono
+	if wav.shape[0] == 2:
+		wav = torch.mean(wav, dim=0).unsqueeze(0)
+	wav = wav[0]  # (1, length) -> (length,)
+
+	# zero padding to both ends
+	length_adj = unit_length - len(wav)
+	if length_adj > 0:
+		half_adj = length_adj // 2
+		wav = F.pad(wav, (half_adj, length_adj - half_adj))
+
+	# random crop unit length wave
+	length_adj = len(wav) - unit_length
+	start = random.randint(0, length_adj) if length_adj > 0 else 0
+	wav = wav[start:start + unit_length]
+
+	torchaudio.save('wav.wav', wav1.unsqueeze(0), sample_rate=16000)
+	wav_tf1 = wav_transform(wav.numpy(), sample_rate=16000)
+	wav_tf1 = torch.tensor(wav_tf1)
+	wav_tf2 = wav_transform(wav.numpy(), sample_rate=16000)
+	wav_tf2 = torch.tensor(wav_tf2)
+	torchaudio.save('wav_tf1.wav', wav_tf1.unsqueeze(0), sample_rate=16000)
+	torchaudio.save('wav_tf2.wav', wav_tf2.unsqueeze(0), sample_rate=16000)
