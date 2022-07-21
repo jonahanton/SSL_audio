cd7196337e13df31600c86b2d016010f7eba81f7
jonahanton <jonahlouisanton@gmail.com>
2022-07-21 22:00:50 +0100

    Cleaning up prints

--- run_hyperparameter_sweep.py
+++ run_hyperparameter_sweep.py
@@ -80,12 +80,13 @@ def objective(trial):
 	# Train the model 
 	for epoch in range(1, args.epochs+1):
 		model.train()
-		print(f'Training [{epoch}/{args.epochs}]')
+		print(f'Training model with BT objective\n Epoch [{epoch}/{args.epochs}]')
 		loss = train_one_epoch(epoch, model, train_loader, optimizer)
+		print(f'Training epoch {epoch}/{args.epochs} finished')
 		# Fit a linear classifier on frozen embeddings from encoder
 		score = eval(model.encoder, eval_train_loader, eval_val_loader, eval_test_loader)
 		trial.report(score, epoch)
-		print(f'After epoch [{epoch}/{args.epochs}] test score: {score:.4f}')
+		print(f'After epoch [{epoch}/{args.epochs}]\tTest score: {score:.4f}')
 
 		# Handle pruning based on the intermediate value
 		if trial.should_prune():
@@ -116,14 +117,15 @@ def get_embeddings(model, data_loader):
 
 def eval(model, train_loader, val_loader, test_loader):
 	
-	print('\nExtracting embeddings')
+	print('Extracting embeddings from frozen backbone')
 	start = time.time()
 	X_train, y_train = get_embeddings(model, train_loader)
 	X_val, y_val = get_embeddings(model, val_loader)
 	X_test, y_test = get_embeddings(model, test_loader)
-	print(f'Done\nTime elapsed = {time.time() - start:.2f}s')
+	print(f'Done\tTime elapsed = {time.time() - start:.2f}s')
 
 	print('Fitting linear classifier')
+	start = time.time()
 	clf = TorchMLPClassifier(
 		hidden_layer_sizes=(),
 		max_iter=50,
@@ -134,13 +136,16 @@ def eval(model, train_loader, val_loader, test_loader):
 	clf.fit(X_train, y_train, X_val=X_val, y_val=y_val)
 	
 	score = clf.score(X_test, y_test)
+	print(f'Done\tTime elapsed = {time.time() - start:.2f}s')
+
 	return score
 
 
 def train_one_epoch(epoch, model, data_loader, optimizer):
 	model.train()
-	total_loss, total_num, train_bar = 0, 0, tqdm(data_loader)
-	for data_tuple in train_bar:
+	total_loss, total_num = 0, 0
+	start = time.time()
+	for data_tuple in data_loader:
 		(pos_1, pos_2), _ = data_tuple
 		pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
 
@@ -152,9 +157,8 @@ def train_one_epoch(epoch, model, data_loader, optimizer):
 
 		total_num += args.batch_size
 		total_loss += loss.item() * args.batch_size
-
-		train_bar.set_description(f'BT Training Epoch: [{epoch}/{args.epochs}] '
-								  f'Loss: {total_loss /total_num :.4f}')
+		
+	print(f'Done\tTime elapsed = {time.time() - start:.2f}s')
 
 	return total_loss / total_num
 
@@ -196,13 +200,14 @@ if __name__ == '__main__':
 
 	args = get_std_config()
 	parser = argparse.ArgumentParser(description='Hyperparameter tuning')
-	parser.add_argument('--tune', nargs='+', type=str, choices=HYPERPARAMETERS)
+	parser.add_argument('--tune', nargs='+', type=str, default=['lr'], choices=HYPERPARAMETERS)
+	parser.add_argument('--n_trials', type=int, default=5)
 	cfg = parser.parse_args()
-	tune = cfg.tune
+	tune, n_trials = cfg.tune, cfg.n_trials
 
 	optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
 	study = optuna.create_study(direction='maximize')
-	study.optimize(objective, n_trials=5)
+	study.optimize(objective, n_trials=n_trials)
 
 	pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
 	complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
