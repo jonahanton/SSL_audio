59b01e2072e0966cd4e93af27e0e4c52a69fdfdc
jonahanton <jonahlouisanton@gmail.com>
2022-07-22 14:13:44 +0100

    flop counter

---
+++ utils/flop_counter.py
@@ -0,0 +1,32 @@
+import torch 
+import torch.nn as nn
+from fvcore.nn import FlopCountAnalysis
+
+from models import resnet, mae
+from model import ViT, AudioNTT2022
+
+# resnet50
+r50 = resnet.resnet50()
+r50.fc = nn.Identity()
+# resnet50 (ReGP+N.RF)
+r50_ReGP_NRF = resnet.resnet50_ReGP_NRF()
+r50_ReGP_NRF.fc = nn.Identity()
+# audiontt
+audiontt = AudioNTT2022()
+# vit[tiny, small, base]
+vit_tiny = ViT('tiny')
+vit_small = ViT('small')
+vit_base = ViT('base')
+
+archs = {
+    'resnet50':r50, 'resnet50_ReGP_NRF':r50_ReGP_NRF,
+    'audiontt':audiontt,
+    'vit_tiny':vit_tiny, 'vit_small':vit_small, 'vit_base':vit_base,
+}
+
+x = torch.randn((64, 1, 64, 96))
+for k, v in archs.items():
+    print(k)
+    flops = FlopCountAnalysis(v, x)
+    print(flops.total())
+    print(flops.by_module())
