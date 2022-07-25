58a81ffe4e88bd67f8af0c8688e3a3e0050ebd21
jonahanton <jonahlouisanton@gmail.com>
2022-07-25 16:36:49 +0100

    Bug in convstem

--- models/mae.py
+++ models/mae.py
@@ -19,47 +19,48 @@ from models.pos_embed import get_2d_sincos_pos_embed, get_sinusoid_encoding_tabl
 
 
 class ConvStem(nn.Module):
-    """ 
-    ConvStem, from Early Convolutions Help Transformers See Better, Tete et al. https://arxiv.org/abs/2106.14881
+	""" 
+	ConvStem, from Early Convolutions Help Transformers See Better, Tete et al. https://arxiv.org/abs/2106.14881
 	Copy-paste from https://github.com/facebookresearch/moco-v3/blob/main/vits.py
-    """
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None, flatten=True):
-        super().__init__()
+	"""
+	def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, norm_layer=None, flatten=True):
+		super().__init__()
 
-        assert patch_size == 16, 'ConvStem only supports patch size of 16'
-        assert embed_dim % 8 == 0, 'Embed dimension must be divisible by 8 for ConvStem'
+		img_size = to_2tuple(img_size)
+		patch_size = to_2tuple(patch_size)
 
-        img_size = to_2tuple(img_size)
-        patch_size = to_2tuple(patch_size)
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
-        self.num_patches = self.grid_size[0] * self.grid_size[1]
-        self.flatten = flatten
+		assert tuple(patch_size) == (16, 16), 'ConvStem only supports patch size of 16x16'
+		assert embed_dim % 8 == 0, 'Embed dimension must be divisible by 8 for ConvStem'
 
-        # build stem, similar to the design in https://arxiv.org/abs/2106.14881
-        stem = []
-        input_dim, output_dim = 3, embed_dim // 8
-        for l in range(4):
-            stem.append(nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=2, padding=1, bias=False))
-            stem.append(nn.BatchNorm2d(output_dim))
-            stem.append(nn.ReLU(inplace=True))
-            input_dim = output_dim
-            output_dim *= 2
-        stem.append(nn.Conv2d(input_dim, embed_dim, kernel_size=1))
-        self.proj = nn.Sequential(*stem)
+		self.img_size = img_size
+		self.patch_size = patch_size
+		self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
+		self.num_patches = self.grid_size[0] * self.grid_size[1]
+		self.flatten = flatten
 
-        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+		# build stem, similar to the design in https://arxiv.org/abs/2106.14881
+		stem = []
+		input_dim, output_dim = 3, embed_dim // 8
+		for l in range(4):
+			stem.append(nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=2, padding=1, bias=False))
+			stem.append(nn.BatchNorm2d(output_dim))
+			stem.append(nn.ReLU(inplace=True))
+			input_dim = output_dim
+			output_dim *= 2
+		stem.append(nn.Conv2d(input_dim, embed_dim, kernel_size=1))
+		self.proj = nn.Sequential(*stem)
 
-    def forward(self, x):
-        B, C, H, W = x.shape
-        assert H == self.img_size[0] and W == self.img_size[1], \
-            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
-        x = self.proj(x)
-        if self.flatten:
-            x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
-        x = self.norm(x)
-        return x
+		self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
+
+	def forward(self, x):
+		B, C, H, W = x.shape
+		assert H == self.img_size[0] and W == self.img_size[1], \
+			f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+		x = self.proj(x)
+		if self.flatten:
+			x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
+		x = self.norm(x)
+		return x
 
 
 class AttentionKBiasZero(nn.Module):
