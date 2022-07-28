e19cd850f7af5591ba80782c8e85484434816411
jonahanton <jonahlouisanton@gmail.com>
2022-07-28 16:00:04 +0100

    HEAR scores broken down into sub categories

--- hear/extract_results.py
+++ hear/extract_results.py
@@ -9,28 +9,36 @@ import pprint
 
 BASE_DIR = "/rds/general/user/jla21/ephemeral/hear/embeddings"
 OUTPATH = "/rds/general/user/jla21/home/SSL_audio/hear/results.json"
-TASKS = [
-	"beehive_states_fold0-v2-full",
-	"beehive_states_fold1-v2-full",
-	"beijing_opera-v1.0-hear2021-full",
-	"dcase2016_task2-hear2021-full",
-	"esc50-v2.0.0-full",
-	"fsd50k-v1.0-full",
-	"gunshot_triangulation-v1.0-full",
-	"libricount-v1.0.0-hear2021-full",
-	"maestro-v3.0.0-5h",
-	"mridangam_stroke-v1.5-full",
-	"mridangam_tonic-v1.5-full",
-	"nsynth_pitch-v2.2.3-50h",
-	"nsynth_pitch-v2.2.3-5h",
-	"speech_commands-v0.0.2-5h",
-	"speech_commands-v0.0.2-full",
-	"tfds_crema_d-1.0.0-full",
-	"tfds_gtzan-1.0.0-full",
-	"tfds_gtzan_music_speech-1.0.0-full",
-	"vocal_imitation-v1.1.3-full",
-	"vox_lingua_top10-hear2021-full",
-]
+TASKS = dir(
+	environmental=[
+		"beehive_states_fold0-v2-full",
+		"beehive_states_fold1-v2-full",
+		"esc50-v2.0.0-full",
+		"fsd50k-v1.0-full",
+		"gunshot_triangulation-v1.0-full",
+	],
+	speech=[
+		"speech_commands-v0.0.2-5h",
+		"speech_commands-v0.0.2-full",
+		"tfds_crema_d-1.0.0-full",
+		"vocal_imitation-v1.1.3-full",
+		"vox_lingua_top10-hear2021-full",
+		"libricount-v1.0.0-hear2021-full",	
+	],
+	music=[
+		"beijing_opera-v1.0-hear2021-full",
+		"mridangam_stroke-v1.5-full",
+		"mridangam_tonic-v1.5-full",
+		"nsynth_pitch-v2.2.3-50h",
+		"nsynth_pitch-v2.2.3-5h",
+		"tfds_gtzan-1.0.0-full",
+		"tfds_gtzan_music_speech-1.0.0-full",
+	],
+	other=[
+		"dcase2016_task2-hear2021-full",
+		"maestro-v3.0.0-5h",
+	],
+)
 
 
 def extract_task_score(model_dir, task):
@@ -53,10 +61,15 @@ def extract_task_score(model_dir, task):
 def extract_model_scores(model_dir):
 
 	scores = {}
-	for task in TASKS:
-		score = extract_task_score(model_dir, task)
-		if score is not None:
-			scores[task] = score
+	for task_type, tasks in TASKS.items():
+		for task in tasks:
+			score = extract_task_score(model_dir, task)
+			if score is not None:
+				scores.setdefault(task_type, {})
+				scores[task_type][task] = score
+		avg = np.mean(list(scores[task_type].values()))
+		if math.isfinite(avg):
+			scores[task_type]["AVERAGE"] = avg
 	return scores
 
 
@@ -67,9 +80,6 @@ def extract_all():
 	for model_dir in tqdm(model_dirs):
 		model_name = model_dir.strip("/").split("/")[-1]
 		all_scores[model_name] = extract_model_scores(model_dir)
-		avg = np.mean(list(all_scores[model_name].values()))
-		if math.isfinite(avg):
-			all_scores[model_name]["AVERAGE"] = avg
 
 	with open(OUTPATH, "w") as jsonfile:
 		json.dump(all_scores, jsonfile, indent=4)
