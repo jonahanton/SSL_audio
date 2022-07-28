d04f3222e912fa2b50620b91f7edf08aad5a6646
jonahanton <jonahlouisanton@gmail.com>
2022-07-28 12:25:41 +0100

    Finished script to extract HEAR resutls

--- hear/extract_results.py
+++ hear/extract_results.py
@@ -1,12 +1,14 @@
-import json 
-import os 
-import fire
-import glob 
+import json
+import os
+import glob
 from tqdm import tqdm
-import pprint 
+import numpy as np
+import math
+import pprint
 
 
 BASE_DIR = "/rds/general/user/jla21/ephemeral/hear/embeddings"
+OUTPATH = "/rds/general/user/jla21/home/SSL_audio/hear/results.json"
 TASKS = [
 	"beehive_states_fold0-v2-full",
 	"beehive_states_fold1-v2-full",
@@ -34,15 +36,17 @@ TASKS = [
 def extract_task_score(model_dir, task):
 
 	results_json = os.path.join(*[model_dir, "hear.sample", task, "test.predicted-scores.json"])
-	with open(results_json, "r") as jsonfile:
-		print(results_json)
-		results = json.load(jsonfile)
+	try:
+		with open(results_json, "r") as jsonfile:
+			results = json.load(jsonfile)
+	except FileNotFoundError:
+		return
 
 	if "test" in results:
 		score = results["test"]["test_score"]
 	elif "aggregated_scores" in results:
 		score = results["aggregated_scores"]["test_score_mean"]
-	
+
 	return score
 
 
@@ -50,20 +54,27 @@ def extract_model_scores(model_dir):
 
 	scores = {}
 	for task in TASKS:
-		scores[task] = extract_task_score(model_dir, task)
+		score = extract_task_score(model_dir, task)
+		if score is not None:
+			scores[task] = score
 	return scores
 
 
 def extract_all():
-	
+
 	model_dirs = glob.glob(BASE_DIR + "/*/")
 	all_scores = {}
 	for model_dir in tqdm(model_dirs):
-		all_scores[model_dir.split("/")[-1]] = extract_model_scores(model_dir)
+		model_name = model_dir.strip("/").split("/")[-1]
+		all_scores[model_name] = extract_model_scores(model_dir)
+		avg = np.mean(list(all_scores[model_name].values()))
+		if math.isfinite(avg):
+			all_scores[model_name]["AVERAGE"] = avg
 
-	pprint.pprint(all_scores)
-		 
+	with open(OUTPATH, "w") as jsonfile:
+		json.dump(all_scores, jsonfile, indent=4)
+
+
 
 if __name__ == "__main__":
 	extract_all()
- 
