17c3ad21658dc499f4a7b551f13902b95f38c08d
jonahanton <jonahlouisanton@gmail.com>
2022-08-04 22:15:08 +0100

    Option for Gnoise + Mixup

--- utils/transforms.py
+++ utils/transforms.py
@@ -7,13 +7,13 @@ import augmentations
 class AudioPairTransform(nn.Module):
 	
 	def __init__(self, args, train_transform=True, pair_transform=True, 
-				 mixup_ratio=0.2, gauss_noise_ratio=0.3, virtual_crop_scale=(1.0, 1.5)):
+				 mixup_ratio=0.2, gauss_noise_ratio=0.2, virtual_crop_scale=(1.0, 1.5)):
 		super().__init__()
 		if train_transform is True:
 			transforms = []
 			if args.mixup:
 				transforms.append(augmentations.MixupBYOLA(ratio=mixup_ratio))
-			elif args.Gnoise:
+			if args.Gnoise:
 				transforms.append(augmentations.MixGaussianNoise(ratio=gauss_noise_ratio))
 			if args.RRC:
 				transforms.append(augmentations.RandomResizeCrop(virtual_crop_scale=virtual_crop_scale))
